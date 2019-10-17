3254144fc4fd9eb4579a602a5f34e303ab36115e
JoelHastenreiter <55096566+JoelHastenreiter@users.noreply.github.com>
2019-10-16 22:30:43 -0500

    final

--- script.js
+++ script.js
@@ -1,11 +1,13 @@
 var control = 0;
 var margen = 0;
-
+var imagenes = document.getElementsByClassName("slider-slide");
 function derecha() {
     if (control > -2) {
         margen -= 640;
         document.getElementById("wrapper").style.marginLeft = margen + "px";
         control--;
+        puntosActivos();
+        flechas();
     }
 
 }
@@ -14,49 +16,60 @@ function izquierda() {
         margen += 640;
         document.getElementById("wrapper").style.marginLeft = margen + "px";
         control++;
+        puntosActivos();
+        flechas();
     }
 
 }
 function puntoUno() {
     margen = 0;
+    control = 0;
     document.getElementById("wrapper").style.marginLeft = margen + "px";
+    puntosActivos();
+    flechas();
 }
 function puntoDos() {
     margen = -640;
+    control = -1;
     document.getElementById("wrapper").style.marginLeft = margen + "px";
+    puntosActivos();
+    flechas();
 }
 function puntoTres() {
     margen = -1280;
+    control = -2;
     document.getElementById("wrapper").style.marginLeft = margen + "px";
+    puntosActivos();
+    flechas();
 }
 function puntosActivos() {
-    if (margen == 0) {
+    if (control == 0) {
         document.getElementById("uno").classList.add("active");
     } else {
         document.getElementById("uno").classList.remove("active");
     }
-    if (margen == -640) {
+    if (control == -1) {
         document.getElementById("dos").classList.add("active");
     } else {
         document.getElementById("dos").classList.remove("active");
     }
-    if (margen == -1280) {
+    if (control == -2) {
         document.getElementById("tres").classList.add("active");
     } else {
         document.getElementById("tres").classList.remove("active");
     }
 }
-function esconderFlechas() {
-    if (margen == 0) {
+function flechas() {
+    if (control == 0) {
         document.getElementById("left").style.visibility = "hidden";
     } else {
         document.getElementById("left").style.visibility = "visible";
     }
-    if (margen == -1280) {
+    if (control == -2) {
         document.getElementById("right").style.visibility = "hidden";
     } else {
         document.getElementById("right").style.visibility = "visible";
     }
 }
 puntosActivos();
-esconderFlechas();
+flechas();
